edfcbe5bc7faa59d1c32bace9b7dfd41552c984f
Pegah Eizadkhah <peizadkh@gmail.com>
2018-08-02 10:39:58 -0400

    Sets up container component to accept the store

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -1,7 +1,8 @@
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import * as fromStore from '../../store';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
 import { Pizza } from '../../models/pizza.model';
-import { PizzasService } from '../../services/pizzas.service';
 
 @Component({
   selector: 'products',
@@ -30,11 +31,16 @@ import { PizzasService } from '../../services/pizzas.service';
 export class ProductsComponent implements OnInit {
   pizzas: Pizza[];
 
-  constructor(private pizzaService: PizzasService) {}
+  //can only select things form the store that correspond/exist in the productState
+  constructor(private store: Store<fromStore.productsState>) {}
 
   ngOnInit() {
-    this.pizzaService.getPizzas().subscribe(pizzas => {
-      this.pizzas = pizzas;
+    /*lets us access properties from our store
+    creates a selector with a top level prop of 'products' so that
+    we can jump a level down into our data structure to access different props*/
+    this.store.select<any>('products').subscribe(state => {
+      console.log(state);
     });
+    
   }
 }

6e5ac7dfdf094f151b12006dbccec32a07eac16a
Pegah Eizadkhah <peizadkh@gmail.com>
2018-08-05 21:51:21 -0400

    Modifies container component to subscribe to the state selectors

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -17,11 +17,11 @@ import { Pizza } from '../../models/pizza.model';
         </a>
       </div>
       <div class="products__list">
-        <div *ngIf="!((pizzas)?.length)">
+        <div *ngIf="!((pizzas$ | async)?.length)">
           No pizzas, add one to get started.
         </div>
         <pizza-item
-          *ngFor="let pizza of (pizzas)"
+          *ngFor="let pizza of (pizzas$ | async)"
           [pizza]="pizza">
         </pizza-item>
       </div>
@@ -29,19 +29,23 @@ import { Pizza } from '../../models/pizza.model';
   `,
 })
 export class ProductsComponent implements OnInit {
-  pizzas: Pizza[];
+  pizzas$: Observable<Pizza[]>;
 
   //can only select things form the store that correspond/exist in the productState
   constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    /*lets us access properties from our store
+    /*1st approach: lets us access properties from our store
     creates a selector with a top level prop of 'products' so that
-    we can jump a level down into our data structure to access different props*/
-    // "products" matches what's in 'forFeature' method
+    we can jump a level down into our data structure to access different props*
+    "products" matches what's in 'forFeature' method
     this.store.select<any>('products').subscribe(state => {
       console.log(state);
-    });
-    
+    });*/
+    /*2nd approach: observable driven:
+    The store will call the select fn and return the selectors, 
+    which will return that single slice of state which we are asking for. 
+    It will then bind that to the pizzas$ which we are subscribed to with the async pipe*/ 
+    this.pizzas$ = this.store.select(fromStore.getAllPizzas)
   }
 }

b8c492565f4fea50cf8d76fe5c4839e8c7737634
Pegah Eizadkhah <peizadkh@gmail.com>
2018-09-30 11:10:30 -0400

    Adds visualization functionality for default pizza vs creating a new pizza on product-items page

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import { tap } from 'rxjs/operators';
 import * as fromStore from '../../store';
 
 import { Pizza } from '../../models/pizza.model';
@@ -38,7 +39,17 @@ export class ProductItemComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.pizza$ = this.store.select(fromStore.getSelectedPizza);
+    /*tap() operator lets us step out of an observable stream so that
+    what happens there is not part of the stream*/
+    this.pizza$ = this.store.select(fromStore.getSelectedPizza).pipe(
+      tap((pizza: Pizza) => {
+        const pizzaExists = !!(pizza && pizza.toppings);
+        const toppings = pizzaExists ? 
+        pizza.toppings.map(topping => topping.id) //if pizza exists grab all of the topping ids
+        : []; //if not return an empty array
+        this.store.dispatch(new fromStore.VisualizeToppings(toppings));
+      })
+    );
     this.toppings$ = this.store.select(fromStore.getAllToppings);
     this.visualise$ = this.store.select(fromStore.getPizzaVisualized);
   }

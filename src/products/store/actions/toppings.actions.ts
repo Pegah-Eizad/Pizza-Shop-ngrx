e8aba4028492f9381d63e52afffdbd94f93fe261
Pegah Eizadkhah <peizadkh@gmail.com>
2018-09-30 09:29:17 -0400

    Implements VisualizeToppigns action type

--- src/products/store/actions/toppings.actions.ts
+++ src/products/store/actions/toppings.actions.ts
@@ -1,10 +1,10 @@
 import { Action } from '@ngrx/store';
-
 import { Topping } from '../../models/topping.model';
 
 export const LOAD_TOPPINGS = '[Products] Load Toppings';
 export const LOAD_TOPPINGS_FAIL = '[Products] Load Toppings Fail';
 export const LOAD_TOPPINGS_SUCCESS = '[Products] Load Toppings Success';
+export const VISUALIZE_TOPPINGS = '[Products] Visualize Toppings';
 
 //action creators
 export class LoadToppings implements Action {
@@ -21,5 +21,10 @@ export class LoadToppingsSuccess implements Action {
     constructor (public payload: any) {}
 }
 
+export class VisualizeToppings implements Action {
+    readonly type = VISUALIZE_TOPPINGS;
+    constructor(public payload: number[]) {}
+}
+
 //action types
-export type ToppingsAction = LoadToppings | LoadToppingsSuccess | LoadToppingsFail;
+export type ToppingsAction = LoadToppings | LoadToppingsSuccess | LoadToppingsFail | VisualizeToppings;

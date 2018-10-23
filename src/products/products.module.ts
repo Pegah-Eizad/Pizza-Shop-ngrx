ea5d6f809b96434e85a13fca17372345b5388f47
Pegah Eizadkhah <peizadkh@gmail.com>
2018-10-23 18:49:20 -0400

    Adds state guard to ng module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -16,14 +16,19 @@ import * as fromContainers from './containers';
 // services
 import * as fromServices from './services';
 
+// guards 
+import * as fromGuards from './guards';
+
 // routes
 export const ROUTES: Routes = [
   {
     path: '',
+    canActivate: [fromGuards.PizzasGuard],
     component: fromContainers.ProductsComponent,
   },
   {
     path: 'new',
+    canActivate: [fromGuards.PizzasGuard],
     component: fromContainers.ProductItemComponent,
   },
   {
@@ -41,7 +46,7 @@ export const ROUTES: Routes = [
     StoreModule.forFeature('products', reducers), //attach storeModule (products module) to root Store
     EffectsModule.forFeature(effects)
   ],
-  providers: [...fromServices.services],
+  providers: [...fromServices.services, ...fromGuards.guards],
   declarations: [...fromContainers.containers, ...fromComponents.components],
   exports: [...fromContainers.containers, ...fromComponents.components],
 })

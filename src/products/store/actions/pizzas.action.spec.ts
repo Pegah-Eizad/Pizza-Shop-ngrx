867d68a94f69c94d865816ca03705d2519beb609
Pegah Eizadkhah <peizadkh@gmail.com>
2018-11-13 06:29:23 -0500

    Implements Pizzas Load Success unit test

--- src/products/store/actions/pizzas.action.spec.ts
+++ src/products/store/actions/pizzas.action.spec.ts
@@ -12,16 +12,24 @@ describe('Pizzas Actions', () => {
                 });
             });
         });
-    });
-
-    describe('LoadPizzas Actions', () => {
-        describe('LoadPizzas', () => {
+        describe('LoadPizzasFail', () => {
             it('should create an action', () => {
                 const payload = { message: 'Load Error'};
                 const action = new pizzaActions.LoadPizzasFail(payload);
-
+    
                 expect({...action}).toEqual({
                     type: pizzaActions.LOAD_PIZZAS_FAIL,
+                    payload
+                });
+            });
+        });
+        describe('LoadPizzasSuccess', () => {
+            it('should create an action', () => {
+                const payload = { message: 'Load Success'};
+                const action = new pizzaActions.LoadPizzasSuccess(payload);
+    
+                expect({...action}).toEqual({
+                    type: pizzaActions.LOAD_PIZZAS_SUCCESS,
                     payload
                 });
             });

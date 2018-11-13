05ccf5a3556adbd5f4313c5c62b3f31e8d6c8bd8
Pegah Eizadkhah <peizadkh@gmail.com>
2018-11-13 06:18:03 -0500

    Implements Load Pizzas Fail action test

--- src/products/store/actions/pizzas.action.spec.ts
+++ src/products/store/actions/pizzas.action.spec.ts
@@ -13,4 +13,18 @@ describe('Pizzas Actions', () => {
             });
         });
     });
+
+    describe('LoadPizzas Actions', () => {
+        describe('LoadPizzas', () => {
+            it('should create an action', () => {
+                const payload = { message: 'Load Error'};
+                const action = new pizzaActions.LoadPizzasFail(payload);
+
+                expect({...action}).toEqual({
+                    type: pizzaActions.LOAD_PIZZAS_FAIL,
+                    payload
+                });
+            });
+        });
+    });
 });
